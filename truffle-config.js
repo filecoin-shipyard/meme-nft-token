1b24ef3164535aa731ef19d09ebf59140a563c21
vasa <vasa@signy.io>
2020-09-05 14:34:59 +0530

    fix: add solc compiler version in truffle config
    License: MIT
    Signed-off-by: Vaibhav Saini <vasa@dappkit.io>

--- truffle-config.js
+++ truffle-config.js
@@ -9,4 +9,9 @@ module.exports = {
       network_id: "*", // Match any network id
     },
   },
+  compilers: {
+    solc: {
+      version: "0.6.2", // Fetch exact version from solc-bin (default: truffle's version)
+    },
+  },
 };
